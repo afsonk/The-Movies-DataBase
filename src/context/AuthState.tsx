0659af493884d12e5acdc311cdd1762057baf450
afsonk <q8737501@gmail.com>
2021-06-08 18:11:27 +0300

    added necessary dependencies for firebase

--- src/context/AuthState.tsx
+++ src/context/AuthState.tsx
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from "react"
 import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
 import {auth, database} from "../firebase/firebase"
 import {authContextType, ChildrenProps, SingleMovieResponseType} from "../types/types"
 
